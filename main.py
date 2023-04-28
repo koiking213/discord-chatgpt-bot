8b22855e0b2668f9da533e2fc3ee3931c9509db0
koiking213 <koiking213@gmail.com>
2023-04-28 17:14:54 +0900

    get chat history inside thread

--- main.py
+++ main.py
@@ -10,6 +10,26 @@ from typing import Any, List, Dict, Callable, Coroutine
 import functools
 import asyncio
 import yaml
+from enum import Enum
+
+
+class MessageRole(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
+
+class Message:
+    def __init__(self, role: MessageRole, content: str):
+        self.role = role
+        self.content = content
+
+    def __str__(self):
+        return f"{self.role.value}: {self.content}"
+
+    def to_dict(self):
+        return {"role": self.role.value, "content": self.content}
+
 
 # wrapper
 @typing.no_type_check
@@ -31,16 +51,12 @@ def to_thread(func: Callable[..., Any]) -> Callable[..., Coroutine[Any, Any, Any
     return wrapper
 
 @to_thread
-def get_response(content: str) -> str:
-    initial_prompt="あなたは有能なアシスタントです。"
+def get_response(messages: List[Message]) -> str:
 
     start = time.time()
     response = create_chat_completion(
       model=chat_gpt_model,
-      messages=[
-            {"role": "system", "content": initial_prompt},
-            {"role": "user", "content": content},
-        ],
+      messages=[m.to_dict() for m in messages],
       stream=True
     )
 
@@ -60,18 +76,49 @@ class AssistantClient(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message: discord.Message) -> None:
+        print(f'Message: {message}')
         print(f'Message from {message.author}: {message.content}')
-        if bot_name in [m.name for m in message.mentions]:
-            async with message.channel.typing():
+
+        # reply to mention
+        if bot_name not in [m.name for m in message.mentions]:
+            return
+
+        # ignore bot message
+        if message.author.bot:
+            return
+        
+        messages = []
+        initial_prompt="あなたは有能なアシスタントです。"
+        messages.append(Message(MessageRole.SYSTEM, initial_prompt))
+        async with message.channel.typing():
+            if isinstance(message.channel, discord.Thread):
+                chat_messages = []
+                async for old_message in message.channel.history(limit=10):
+                    pattern = r'^<.*?>\s'
+                    content = re.sub(pattern, "", old_message.content)
+                    if old_message.author.bot:
+                        role = MessageRole.ASSISTANT
+                    else:
+                        role = MessageRole.USER
+                    new_message = Message(role, content)
+                    chat_messages.append(new_message)
+                    print(new_message)
+                messages.extend(chat_messages[::-1])
+            elif isinstance(message.channel, discord.TextChannel):
                 pattern = r'^<.*?>\s'
                 content = re.sub(pattern, "", message.content)
-                response = await get_response(content)
-                print(response)
-                print(len(response))
-                length = len(response)
-                for i in range(((length-1)//1000)+1):
-                    await message.channel.send(response[i*1000:(i+1)*1000])
+                messages.append(Message(MessageRole.USER, content))
+            else:
+                print("Unknown channel type")
+                return
 
+            response = await get_response(messages)
+            print(response)
+            print(len(response))
+            length = len(response)
+            for i in range(((length-1)//1000)+1):
+                await message.channel.send(response[i*1000:(i+1)*1000])
+    
 def main() -> None:
     intents = discord.Intents.default()
     intents.message_content = True
